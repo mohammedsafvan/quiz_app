578bce3802b79296a8b1d701ef2fae9db7827ad3
Mohammed Safvan <mohammedsafvannk@gmail.com>
2022-11-04 19:16:28 +0530

    Root page completed

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useRef } from "react";
+import { Link } from "react-router-dom";
+import '../styles/Main.css'
 
 const Main = () => {
-  return (
-    <div>Main</div>
-  )
-}
+  const inputRef = useRef();
 
-export default Main
+  return (
+    <div className="container">
+      <h1 className="title text-light">Quiz Application</h1>
+
+      <h3 className="text-light" >Rules</h3>
+      <ol>
+        <li>10 Questions</li>
+        <li>Each question has 10 Points</li>
+        <li>You should choose from 3 options</li>
+      </ol>
+
+      <form id="form">
+        <input ref={inputRef} type="text" className="userid" placeholder="Username*" />
+      </form>
+
+      <div className="start">
+        <Link className="btn" to={"quiz"}>
+          Start Quiz
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
